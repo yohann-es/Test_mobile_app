658d2caa53c92d5c477f5ad58cc3efe9699c17c2
john <yohannesprog@gmail.com>
2025-08-22 00:25:57 +0300

    testing

--- test_flut_v_one/test/widget_test.dart
+++ test_flut_v_one/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:test_flut_v_one/main.dart';
+import 'package:test_flut_v_one/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
